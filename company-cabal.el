e315329ee9f1351a4917db33c281743f3687bdc4
Iku Iwasa <iku.iwasa@gmail.com>
2015-01-19 23:14:28 +0900

    Add brief commentary

--- company-cabal.el
+++ company-cabal.el
@@ -23,6 +23,13 @@
 
 ;;; Commentary:
 
+;; `company-mode' back-end for `haskell-cabal-mode'.
+;;
+;; Provide completion for section name, field name and some of field values.
+;; Add `company-cabal' to `company-mode' back-ends list.
+;;
+;;     (add-to-list 'company-backends 'company-cabal)
+
 ;;; Code:
 
 (require 'cl-lib)
